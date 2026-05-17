ef190af79600f9fc9aa551d2d7e01af75295337f
Yacine Akli <yacine.akli97@gmail.com>
2026-05-17 21:13:29 +0200

    add my portfolio project

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
 						<img src={WIKISEARCH} alt='Wikipedia search engine' />
 					</div>
 
-					<h3>Wikipedia search engine</h3>
+					<h3>Wikipedia Search Engine</h3>
 
 					<small className='text-light'>JAVA | PYTHON | CSS</small>
 
@@ -27,9 +27,30 @@ const Projects = () => {
 						>
 							GitHub
 						</a>
+					</div>
+				</article>
+				<article className='portfolio__item'>
+					<span className='card__bar' />
+					<div className='portfolio__item-image'>
+						<img src={WIKISEARCH} alt='Wikipedia search engine' />
+					</div>
+
+					<h3>My Portfolio</h3>
+
+					<small className='text-light'>REACT.JS | TYPESCRIPT | CSS</small>
+
+					<div className='portfolio__item-cta'>
+						<a
+							href='https://github.com/yacineak97/yacineak97.github.io'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='btn'
+						>
+							GitHub
+						</a>
 
 						<a
-							href='https://github.com/yacineak97/wikipedia-search-engine'
+							href='https://yacineak97.github.io/'
 							target='_blank'
 							rel='noopener noreferrer'
 							className='btn btn-primary'
